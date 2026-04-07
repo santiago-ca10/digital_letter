74b1b64d8c454bbff12767530f60b4aaa69a5019
SCA10 <santycardal2019@gmail.com>
2026-04-06 20:09:36 -0500

    actualizacion parcial de la pagina teniendo un diseño funcional y el sobre funcionando correctamente

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const envelope = document.getElementById("envelope");
+document.addEventListener("DOMContentLoaded", () => {
+    const envelope = document.getElementById("envelope");
+    const statusText = document.getElementById("status-text");
 
-envelope.addEventListener("click", () => {
-  envelope.classList.toggle("open");
+    let isOpened = false;
+
+    envelope.addEventListener("click", () => {
+        isOpened = !isOpened;
+
+        if (isOpened) {
+            envelope.classList.add("open");
+            statusText.textContent = "Haz clic para cerrar";
+        } else {
+            envelope.classList.remove("open");
+            statusText.textContent = "Haz clic para abrir el sobre";
+        }
+    });
 });
